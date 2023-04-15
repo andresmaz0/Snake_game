d4ff0055e954e8c00ac0c4d81bc8e93ddd96702c
andresmaz0 <amazobo@eafit.edu.co>
2023-04-15 13:16:43 -0500

    Imported class donas

--- app/S_game.py
+++ app/S_game.py
@@ -1,5 +1,6 @@
 import pygame, sys, random
 from class_player import Player
+from class_donas import Donas
 
 #iniciacion de pygame
 pygame.init()
@@ -24,30 +25,14 @@ background = pygame.image.load("app/imagenes/fondo_d.PNG")
 head = pygame.image.load("app/imagenes/head_s.png")
 dona_r = pygame.image.load("app/imagenes/dona_rosa.png")
 
-class Donas (pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = dona_r
-        #obtengo el rectangulo de la imagen head
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(width - self.rect.width)
-        self.rect.y = random.randrange(height - self.rect.height)
-
-    #metodo para reapiricion de dona despues de colision
-    def reaparicion(self,colision):
-        if colision:
-
-            self.rect.x = random.randrange(width - self.rect.width)
-            self.rect.y = random.randrange(height - self.rect.height)
-
 #grupo de sprites
 sprites = pygame.sprite.Group()
-jugador = Player(width=width,height=height,head=head)
+jugador = Player(width=width,height=height,image=head)
 sprites.add(jugador)
 
 #añadiendo las donas
 grupo_donas = pygame.sprite.Group()
-dona_rosa = Donas()
+dona_rosa = Donas(width=width,height=height,image=dona_r)
 grupo_donas.add(dona_rosa)
 
 #bucle del juego
@@ -63,7 +48,8 @@ while True:
 
     #realizando colision de sprites
     colision = pygame.sprite.spritecollide(jugador,grupo_donas,False)
-    dona_rosa.reaparicion(colision)
+    if colision:
+        dona_rosa.reaparicion()
 
     """x_relativa = x % background.get_rect().width
     screen.blit(background, (x_relativa - background.get_rect().width, 0))
