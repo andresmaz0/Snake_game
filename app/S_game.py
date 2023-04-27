c7cab2540577985ca22c626dc2331c1300a20c8a
andresmaz0 <amazobo@eafit.edu.co>
2023-04-27 17:28:47 -0500

    Adding a list to save my body parts and logic to work

--- app/S_game.py
+++ app/S_game.py
@@ -31,8 +31,8 @@ dona_r = pygame.image.load("snake_game/app/imagenes/dona_rosa.png")
 ball_body = pygame.image.load("snake_game/app/imagenes/circulo_body.png")
 
 #Body group
-cuerpo = body(image=ball_body)
 grupo_cuerpo = pygame.sprite.Group()
+cuerpo_completo=[]
 
 #Sprites group
 sprites = pygame.sprite.Group()
@@ -53,14 +53,16 @@ while True:
             sys.exit()
     #Updating sprites
     sprites.update()
+    grupo_cuerpo.update()
     screen.fill((255,255,255))
 
     #Performing sprites collision
     colision = pygame.sprite.spritecollide(jugador,grupo_donas,False)
     if colision:
         dona_rosa.reaparicion()
-        cuerpo = body(image=ball_body)
-        grupo_cuerpo.add(cuerpo)
+        parte_cuerpo = body(image=ball_body)
+        grupo_cuerpo.add(parte_cuerpo)
+        cuerpo_completo.append(parte_cuerpo) 
         n_colisiones+=1
 
     """x_relativa = x % background.get_rect().width
@@ -68,10 +70,18 @@ while True:
     if x_relativa<width:
         screen.blit(background,(x_relativa,0))
     x -= 0.8"""
+    #building the entire body of the snake
+    for i in range(len(cuerpo_completo)-1,0,-1):
+        cuerpo_completo[i].posicion(index_x=cuerpo_completo[i-1].rect.x,index_y=cuerpo_completo[i-1].rect.top)
+
+    #I need always my first elemnt of my body updating it is why the conditio is >0
+    if len(cuerpo_completo)>0:
+        cuerpo_completo[0].posicion(index_x=jugador.rect.x,index_y=jugador.rect.top)
+
+    #Is important the order of which group is drawed first
     grupo_cuerpo.draw(screen)
     grupo_donas.draw(screen)
     sprites.draw(screen)
-    cuerpo.posicion(head_x=jugador.rect.x,head_y=jugador.rect.top,colisiones=n_colisiones)
     jugador.movimiento()
     pygame.display.update()
 
