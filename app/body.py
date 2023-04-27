f74ddcf300d7283d46c020c53edae9491c3c2095
andresmaz0 <amazobo@eafit.edu.co>
2023-04-27 11:33:59 -0500

    Creating a circle

--- app/body.py
+++ app/body.py
@@ -1,5 +1,6 @@
 import pygame, sys, random
 
 class body (pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,screen,color,head_x,head_y):
         super().__init__()
+        self.image = pygame.draw.circle(screen,color,(head_x,head_y),30,0)
