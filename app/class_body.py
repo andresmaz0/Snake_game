1d551b17699fc7a841967b657ef46b6b8004a0cc
andresmaz0 <amazobo@eafit.edu.co>
2023-04-27 12:25:10 -0500

    Adding my parameters

--- app/class_body.py
+++ app/class_body.py
@@ -1,6 +1,15 @@
 import pygame, sys, random
 
 class body (pygame.sprite.Sprite):
-    def __init__(self,screen,color,head_x,head_y):
+    def __init__(self,width:int,height:int,image):
         super().__init__()
-        self.image = pygame.draw.circle(screen,color,(head_x,head_y),30,0)
+        self.image = image
+        self.width = width
+        self.height = height
+        #I get the rectangle of the image head
+        self.rect = self.image.get_rect()
+    
+    """def posicion
+        self.rect.x += self.velocidad_x
+        self.rect.y += self.velocidad_y"""
+        
