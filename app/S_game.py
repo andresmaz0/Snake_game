5644814e2a715fcad52e51721471c3d88b4f4ad1
andresmaz0 <amazobo@eafit.edu.co>
2023-04-27 13:33:53 -0500

    Creating a variable for the number of collitions

--- app/S_game.py
+++ app/S_game.py
@@ -14,6 +14,7 @@ pygame.display.set_caption("Snake Game")
 #Screen movement variables
 x=0
 x_relativa=0
+n_colisiones=0
 
 #Fps
 clock = pygame.time.Clock()
@@ -29,7 +30,6 @@ ball_body = pygame.image.load("snake_game/app/imagenes/circulo_body.png")
 #Sprites group
 sprites = pygame.sprite.Group()
 cuerpo = body(image=ball_body)
-sprites.add(cuerpo)
 jugador = Player(width=width,height=height,image=head)
 sprites.add(jugador)
 
@@ -53,6 +53,8 @@ while True:
     colision = pygame.sprite.spritecollide(jugador,grupo_donas,False)
     if colision:
         dona_rosa.reaparicion()
+        sprites.add(cuerpo)
+        n_colisiones+=1
 
     """x_relativa = x % background.get_rect().width
     screen.blit(background, (x_relativa - background.get_rect().width, 0))
