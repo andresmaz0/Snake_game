3f47c808912a70003b5c444b84475f842f66468f
andresmaz0 <amazobo@eafit.edu.co>
2023-04-27 12:46:56 -0500

    Updating my method posicion

--- app/class_body.py
+++ app/class_body.py
@@ -1,15 +1,15 @@
 import pygame, sys, random
 
 class body (pygame.sprite.Sprite):
-    def __init__(self,head_x:int,head_y:int,image):
+    def __init__(self,image):
         super().__init__()
         self.image = image
-        self.head_x = head_x
-        self.head_y = head_y
         self.rect = self.image.get_rect()
     
     #Updating the position of my body
-    def posicion(self):
+    def posicion(self,head_x:int,head_y:int):
+        self.head_x = head_x
+        self.head_y = head_y
         self.rect.x = self.head_x + 5
         self.rect.y = self.head_y - 30
         
